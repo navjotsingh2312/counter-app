c23a0fca73469b87ed87b7cd7acc95dd6c78c48a
navjotsingh2312 <navjotsingh23199@gmail.com>
2020-08-06 10:39:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title :string= 'counter-app';
+  counter:number=0;
+  handleIncrease=()=>{
+    this.counter=this.counter+1;
+    if(this.counter>10)
+    {
+      
+      this.counter=10;
+    }
+    
+  }
+  handleDecrease=()=>{
+    this.counter=this.counter-1;
+    if(this.counter<0)
+    {
+      this.counter=0;
+    }
+  }
+  handleReset(){
+    this.counter=0;
+  }
 }
